7994570f617f10b3fce1a9380128f8547946ffb5
Georg Ramer <georg.ramer@gmail.com>
2018-03-10 22:17:37 -0500

    ready for PYPi

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
 setup(name='spectroscopy_data',
-     version='0.9',
+     version='0.10',
      description='Provides data to go along with spectroscopy.ramer.at',
-     url='spectroscopy.ramer.at',
+     url='https://github.com/GeorgRamer/spectroscopy_data',
+     download_url='https://github.com/GeorgRamer/spectroscopy_data/archive/0.10.tar.gz',
      author='georg.ramer@gmail.com',
      classifiers=['Development Status :: 4 - Beta',
                   'Intended Audience :: Spectroscopists',
